bfdf5c256c98cf90db176589237b5408d8ba4bf1
DimasRP <drizki80@gmail.com>
2022-08-12 21:50:23 +0700

    update

--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,8 +1,6 @@
 import React from "react"
-import {useNavigate} from "react-router-dom"
 
 const Dashboard = () => {
-    const navigate = useNavigate();
     return (
         <h1>Welcome Dashboard</h1>
     )
