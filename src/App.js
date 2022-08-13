f75f7fd1baf0051a1e2782e95d635e2106ddc300
DimasRP <drizki80@gmail.com>
2022-08-13 11:53:05 +0700

    update dell

--- src/App.js
+++ src/App.js
@@ -6,23 +6,38 @@ import Register from './Pages/Register';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
 import Protected from './HOC/ProtectedRoute';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [user, setlogin] = useEffect(null)
+  const [user, setLogin] = useState(false)
+
+  const checkIfLogin = () => {
+    const token = localStorage.getItem("Token");
+    if (!token) {
+      setLogin(false);
+    } else {
+      setLogin(true);
+    }
+  };
+  useEffect(() => {
+    checkIfLogin();
+  }, []);
+
   return (
     <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route 
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route 
           path='/dashboard' 
           element={
-            <Protected user={false}>
+            <Protected user={user}>
               <Dashboard/>
             </Protected>
           }
-        />
+        /> */}
       </Routes>
   );
 }
--- src/Pages/Dashboard/index.js
+++ src/Pages/Dashboard/index.js
@@ -1,8 +1,66 @@
-import React from "react"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import './dashboard.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const Dashboard = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const [data, setData] = useState([])
+    useEffect(() => {
+        getData()
+    },[])
+
+    const getData = () => {
+        axios
+        .get("https://reqres.in/api/users?page=2")
+        .then((res) => setData(res.data.data))
+        .catch((err) => console.log(err))
+    }
+    const handleDelete = (id) =>{
+        axios.delete(`https://reqres.in/api/users/${id}`)
+        .then((res) => {
+            if (res.status == 204){
+                setShow(false)
+            }
+        })
+        .catch()
+        console.log(id);
+    }
     return (
-        <h1>Welcome Dashboard</h1>
+        <div>
+          <h1>Dashboard</h1>
+          <div className="item">
+          {data.map((item) => (
+            <div>
+                <p>{item.first_name}</p>
+                <img src={item.avatar}/>
+                <div>
+                    <Button variant="primary" onClick={handleShow}>
+                    Edit
+                    </Button>
+                    <Button variant="danger" onClick={handleShow}>
+                    Delete
+                    </Button>
+                </div>
+                <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{item.first_name}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={() => handleDelete(item.id)}>
+                        Delete
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            </div>
+          ))}
+          </div>
+        </div>
     )
 }
 
